e30939e70292463c07979f5a1c1a49a673f5e0c0
Lucas Germano <lucas.germano@planb.com.br>
2016-06-20 17:20:35 -0300

    Create app.js to config app

---
+++ app/app.js
@@ -0,0 +1,65 @@
+// app.js
+// create our angular app and injects
+// =============================================================================
+angular.module('FormApp', ['ngAnimate', 'ui.router', 'ngMaterial', 'SWModule', 'GotModule', 'FoodModule'])
+
+
+    .config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+
+        $stateProvider
+            // App routing
+            .state('form', {
+                url: '/',
+                templateUrl: 'formBase.html',
+                controller: 'formController',
+            })
+
+            .state('form.profile', {
+                url: '/profile',
+                templateUrl: 'modules/profile/profile.view.html'
+            })
+
+            .state('form.starwarsmovie', {
+                url: '/starwarsmovie',
+                templateUrl: 'modules/starwars/starwars.view.html',
+                controller: 'SWController as vm'
+            })
+ 
+            .state('form.gameofthrones', {
+                url: '/gameofthrones',
+                templateUrl: 'modules/got/got.view.html',
+                controller: 'GotController as vm'
+            })
+
+            .state('form.food', {
+                url: '/food',
+                templateUrl: 'modules/food/food.view.html',
+                controller: 'FoodController as vm'
+            })
+
+            .state('form.review', {
+                url: '/review',
+                templateUrl: 'review.html',
+            });
+
+        // catch all route
+        // send users to the form page
+        $urlRouterProvider.otherwise('//profile');
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('red');
+    })
+
+    // our controller for the form
+    // ============================
+    .controller('formController', function($scope) {
+
+        // we will store all of our form data in this object
+        $scope.formData = {};
+
+        // function to process the form
+        $scope.processForm = function() {
+            alert('Thank you! “Powerful you have become, the dark side I sense in you.” - Yoda!');
+        };
+
+    });
